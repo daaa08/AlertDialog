93e2cda7ff1c44ca58c51ee1d2b68c610c17dc39
박다정 <daaa08@naver.com>
2017-08-17 19:57:45 +0900

    exit 이벤트 처리

--- app/src/main/java/com/da08/alertdialog/MainActivity.java
+++ app/src/main/java/com/da08/alertdialog/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
     Button listbtn, exitbtn;
 
-    private String[] items = {"워너원","태양","쇼미더머니","로꼬"};   // list에 들어갈 내
+    private String[] items = {"워너원","태양","쇼미더머니","로꼬"};   // list에 들어갈 내용
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +22,11 @@ public class MainActivity extends AppCompatActivity {
         listbtn = (Button)findViewById(R.id.listbtn);
         exitbtn = (Button)findViewById(R.id.exitbtn);
 
+        // list 리스너
         listbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);  // alert창 띄우기
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);  // alert창 띄우기 (MainActivity에 종속)
                 builder.setTitle("가수");   // alert창 title
                 builder.setItems(items, new DialogInterface.OnClickListener() {   // alert창의 내용 넣기
                     @Override
@@ -37,5 +38,34 @@ public class MainActivity extends AppCompatActivity {
                 alertDialog.show();  // 보여주기
             }
         });
+
+        // exit 리스너
+        exitbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                 builder.setMessage("종료할꺼임?");   // builder에 msg 설정
+                builder.setTitle("종료 알림창")
+                        .setCancelable(false)  // 사용자가 임의로 종료할 수 없도록 함
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                finish();   // 전체 종료
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener(){
+
+                            @Override
+                            public void onClick(DialogInterface dialog, int i) {
+                                dialog.cancel();  // alert창만 꺼짐
+                            }
+                        });
+                AlertDialog alert = builder.create();  // 객체 할당
+                alert.setTitle("종료알림창");
+                alert.show();  // 보여주기
+
+            }
+        });
+
     }
 }
